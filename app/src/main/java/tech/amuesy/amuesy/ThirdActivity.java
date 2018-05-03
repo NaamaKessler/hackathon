387ab440e67a755718e8602b0d89afc7badfb4bb
elielak <elielak@gmail.com>
2018-05-03 18:16:04 +0300

    2 & 3 Activity

--- app/src/main/java/tech/amuesy/amuesy/ThirdActivity.java
+++ app/src/main/java/tech/amuesy/amuesy/ThirdActivity.java
@@ -6,11 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
 
-
+    double progress_val = 24;
+    TextView tv;
+    SeekBar sb;
 
 
     @Override
@@ -18,18 +22,41 @@ public class ThirdActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
+        // Profile Spinner - need to save answer from user
         Spinner ProfileSpinner = (Spinner) findViewById(R.id.ProfileSpinner);
         ArrayAdapter<String> adapter3_1 = new ArrayAdapter<String>(ThirdActivity.this,
-                android.R.layout.simple_list_item_1,getResources().getStringArray((R.array.Profiles)));
+                android.R.layout.simple_list_item_1, getResources().getStringArray((R.array.Profiles)));
         adapter3_1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ProfileSpinner.setAdapter(adapter3_1);
 
 
+        // Level Spinner - need to save answer from user
         Spinner LevelSpinner = (Spinner) findViewById(R.id.LevelSpinner);
         ArrayAdapter<String> adapter3_2 = new ArrayAdapter<String>(ThirdActivity.this,
-                android.R.layout.simple_list_item_1,getResources().getStringArray((R.array.Levels)));
+                android.R.layout.simple_list_item_1, getResources().getStringArray((R.array.Levels)));
         adapter3_2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         LevelSpinner.setAdapter(adapter3_2);
+
+
+        //seek Bar
+        sb = (SeekBar) findViewById(R.id.seekBar);
+        tv = (TextView) findViewById(R.id.HoursTxt);
+        sb.setMax(12);
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv.setText(progress + " Hours");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                tv.setText(seekBar.getProgress() + " Hours");
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                tv.setText(seekBar.getProgress() + " Hours");
+            }
+        });
 
 
         Button okBtn = findViewById(R.id.okBtn);
@@ -39,15 +66,9 @@ public class ThirdActivity extends AppCompatActivity {
                 Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(startIntent);
             }
-
-
         });
 
 
-
-
     }
-
-    //comment
 
 }
