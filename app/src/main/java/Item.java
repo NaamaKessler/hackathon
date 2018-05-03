c5e238b97893de18ab6a67d81dd9afbab01bb153
Naama Kessler <naamak10@gmail.com>
2018-05-03 22:11:32 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/build.gradle

--- app/src/main/java/Item.java
+++ app/src/main/java/Item.java
@@ -1,4 +1,24 @@
+
+import java.util.List;
+
 public class Item {
+    private final String title, artist, period, childInfo, adultInfo, expertInfo;
+    private final int floor, room, childGrade, adultGrade, expertGrade;
+
+
+    public Item(List attributes){
+        this.title = (String)attributes.get(0);
+        this.artist = (String)attributes.get(1);
+        this.period = (String)attributes.get(2);
+        this.floor = (int)attributes.get(3);
+        this.room = (int)attributes.get(4);
+        this.childGrade = (int)attributes.get(5);
+        this.adultGrade = (int)attributes.get(6);
+        this.expertGrade = (int)attributes.get(7);;
+        this.childInfo = (String)attributes.get(8);
+        this.adultInfo = (String)attributes.get(4);
+        this.expertInfo = (String)attributes.get(4);
+    }
 
     public void presentItem(){
 
--- app/src/main/java/TourFactory.java
+++ app/src/main/java/TourFactory.java
@@ -1,5 +1,6 @@
 // ------------------------- imports -------------------------
 import com.jniwrapper.win32.jexcel.Application;
+import com.jniwrapper.win32.jexcel.Cell;
 import com.jniwrapper.win32.jexcel.ExcelException;
 import com.jniwrapper.win32.jexcel.Range;
 import com.jniwrapper.win32.jexcel.Workbook;
@@ -16,45 +17,42 @@ public class TourFactory {
     private static final int ITEM_OBSERVATION_DURATION = 10;
 
     /**
-     * Creates tour accoding to visitor specifications.
+     * Creates tour according to visitor specifications.
      * @param visitorType adult/child/expert
-     * @param tourDuration minuntes.
+     * @param tourDuration minutes.
      * @return Tour obj.
      */
-    public ArrayList<Tour> getTour(String visitorType, int tourDuration){
-        ArrayList<Tour> Tour;
+    public ArrayList<Item> getTour(String visitorType, int tourDuration){
+        ArrayList<Item> tour = new ArrayList<>();
         int numItems = (int)Math.floor(tourDuration/ITEM_OBSERVATION_DURATION);
 
         try {
             // open excel and sort items according to visitorType ranking:
             Application application = new Application();
-            if (!application.isVisible()) {
-                application.setVisible(true);
-            }
+//            if (!application.isVisible()) {
+//                application.setVisible(true);
+//            }
             File xlsFile = new File("Data1.xlsx");
-            Workbook workbook = application.openWorkbook(xlsFile, true, null);
+            Workbook workbook = application.openWorkbook(xlsFile, true, "");
             Worksheet sheet = workbook.getActiveWorksheet();
             Range data = sheet.getUsedRange();
             data.sort(visitorType, false, false);
 
             // select k-best:
             for (int i = 0; i < numItems; i++){
-                Range tourData = sheet.getRow(i);
-//                tourData.get
+                java.util.List itemData = sheet.getRow(i).getCells();
+                tour.add(new Item(sheet.getRow(i).getCells()));
             }
-
+            return tour;
 
         } catch (ExcelException e) {
             e.printStackTrace();
+            return null;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            return null;
         }
 
-
-//        Worksheet customSheet = workbook.getWorksheet("Custom sheet");
-
-
-        return null;
 
     }
 
