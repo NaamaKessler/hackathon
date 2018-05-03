01c5b633b233bff421485f7ef97eaff42e69f490
elielak <elielak@gmail.com>
2018-05-03 12:58:29 +0300

    2 & 3 Activity

--- app/src/main/java/tech/amuesy/amuesy/ThirdActivity.java
+++ app/src/main/java/tech/amuesy/amuesy/ThirdActivity.java
@@ -2,6 +2,8 @@ package tech.amuesy.amuesy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        AutoCompleteTextView MusNameTxt = (AutoCompleteTextView)findViewById(R.id.MusNameTxt);
+        ArrayAdapter<String> adapter = new ArrayAdapter<string>(this,R.layout.support_simple_spinner_dropdown_item,Museum_list);
+        MusNameTxt.setAdapter(adapter);
     }
+
+    private static String[] Museum_list = new String[]{"Mus1","Haifa","TLV","Jerusalem","Paris","London"};
 }
