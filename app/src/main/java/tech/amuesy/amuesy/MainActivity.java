01ae130e6b8d66be6642b9dfc31078336d362424
SergeyHazanov <38078324+SergeyHazanov@users.noreply.github.com>
2018-05-03 13:47:19 +0300

    Delete pics

--- app/src/main/java/tech/amuesy/amuesy/MainActivity.java
+++ app/src/main/java/tech/amuesy/amuesy/MainActivity.java
@@ -1,7 +1,8 @@
 package tech.amuesy.amuesy;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
