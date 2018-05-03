80460858982425575a682ce088f98a1110c3c40c
elielak <elielak@gmail.com>
2018-05-03 12:10:44 +0300

    2 & 3 Activity

--- app/src/main/java/tech/amuesy/amuesy/SecondActivity.java
+++ app/src/main/java/tech/amuesy/amuesy/SecondActivity.java
@@ -1,7 +1,10 @@
 package tech.amuesy.amuesy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button firstActivityBtn = (Button) findViewById(R.id.firstActBtn);
+        firstActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
