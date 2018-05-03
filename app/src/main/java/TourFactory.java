5432ac8d87f218fb54643616d53877dd2a08934a
Naama Kessler <naamak10@gmail.com>
2018-05-03 20:30:07 +0300

    added classes

---
+++ app/src/main/java/TourFactory.java
@@ -0,0 +1,38 @@
+
+import com.jniwrapper.win32.jexcel.Application;
+import com.jniwrapper.win32.jexcel.ExcelException;
+import com.jniwrapper.win32.jexcel.Worksheet;
+
+import java.util.ArrayList;
+
+public class TourFactory {
+    private static final int DURATION_MINUTES = 10;
+
+    public ArrayList<Tour> getTour(String visitorType, int tourDuration){
+        ArrayList<Tour> Tour;
+
+
+        Application application = null;
+        try {
+            application = new Application();
+            if (!application.isVisible())
+            {
+                application.setVisible(true);
+            }
+
+        } catch (ExcelException e) {
+            e.printStackTrace();
+        }
+        if (!application.isVisible())
+        {
+            application.setVisible(true);
+        }
+
+//        Worksheet customSheet = workbook.getWorksheet("Custom sheet");
+
+
+        return null;
+
+    }
+
+}
