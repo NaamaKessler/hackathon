956e1599e6160f6005395df23cf89ce67c2bf4e4
SergeyHazanov <38078324+SergeyHazanov@users.noreply.github.com>
2018-05-04 03:04:03 +0300

    Update colors and buttons

--- app/src/main/java/tech/amuesy/amuesy/MusInfoActivity.java
+++ app/src/main/java/tech/amuesy/amuesy/MusInfoActivity.java
@@ -1,7 +1,10 @@
 package tech.amuesy.amuesy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MusInfoActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MusInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mus_info);
+
+        Button infoBackBtn = findViewById(R.id.infoBackBtn);
+
+        infoBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                Intent start1Intent = new Intent(getApplicationContext(),InMuseumActivity.class);
+                startActivity(start1Intent);
+            }
+        });
     }
 }
