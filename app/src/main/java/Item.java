4d1a9777dc61173a87362afb83a7c3d78bb61728
Naama Kessler <naamak10@gmail.com>
2018-05-04 03:02:26 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/build.gradle

--- app/src/main/java/Item.java
+++ app/src/main/java/Item.java
@@ -1,9 +1,8 @@
-
 import java.util.List;
 
 public class Item {
     private final String title, artist, period, childInfo, adultInfo, expertInfo;
-    private final int floor, room, childGrade, adultGrade, expertGrade;
+    private final int floor, room, creationYear;
 
     public Item(List attributes){
         this.title = (String)attributes.get(0);
@@ -11,42 +10,41 @@ public class Item {
         this.period = (String)attributes.get(2);
         this.floor = (int)attributes.get(3);
         this.room = (int)attributes.get(4);
-        this.childGrade = (int)attributes.get(5);
-        this.adultGrade = (int)attributes.get(6);
-        this.expertGrade = (int)attributes.get(7);;
         this.childInfo = (String)attributes.get(8);
         this.adultInfo = (String)attributes.get(9);
         this.expertInfo = (String)attributes.get(10);
+        this.creationYear = (int)attributes.get(11);
     }
 
-    public void presentItem(){
-
+    public String presentItem(String visitorType){
+        //get details:
+        String text = "Title: " + this.title + "\nArtist: " + this.artist + "\nYear of creation: " +
+                this.creationYear + "\nPeriod: " + this.period + "\n\n";
+        // get tailored info:
+        switch (visitorType) {
+            case "Child":
+                text += this.childInfo;
+                break;
+            case "Easy":
+                text += this.childInfo;
+                break;
+            case "Teen":
+                text += this.childInfo;
+                break;
+            case "Intermediate":
+                text += this.childInfo;
+                break;
+            case "Adult":
+                text += this.adultInfo;
+                break;
+            default:
+                text += this.expertInfo;
+        }
+        return text;
     }
 
-    public void presentBackground(){
-
-    }
-
-
-    public int getYear(){
-        return 0;
-    }
-
-    public String getArtistName(){
-        return null;
-    }
-
-    public String ChildrenExp(){
-        return null;
-    }
-
-
-    public String adultsExp(){
-        return null;
-    }
-
-    public String experts() {
-        return null;
+    public String getLocation(){
+        return "Floor: " + this.floor + "Hall no.: " + this.room;
     }
 
 
