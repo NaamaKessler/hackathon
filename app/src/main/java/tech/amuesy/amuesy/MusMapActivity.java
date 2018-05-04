acbe0264d1d76bc100c40f82df115aed893ca2a0
SergeyHazanov <38078324+SergeyHazanov@users.noreply.github.com>
2018-05-04 03:40:00 +0300

    Update colors and buttons

---
+++ app/src/main/java/tech/amuesy/amuesy/MusMapActivity.java
@@ -0,0 +1,25 @@
+package tech.amuesy.amuesy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MusMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musmap);
+
+        Button mapBackBtn = findViewById(R.id.mapBackBtn);
+        mapBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v1) {
+                Intent start1Intent = new Intent(getApplicationContext(),InMuseumActivity.class);
+                startActivity(start1Intent);
+            }
+        });
+    }
+}
